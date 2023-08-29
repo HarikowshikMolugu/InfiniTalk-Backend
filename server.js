63429b093fc4fe411a56671fe79d1a11da30b0e9
Hari kowshik Molugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-08-29 20:41:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 
 // Enable CORS
 app.use(cors());
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // Use body-parser middleware
 
 app.use(express.static("upload"));
