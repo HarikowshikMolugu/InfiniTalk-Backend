9c341405a52656492d11adf8d6956b4ba05c75a9
Hari kowshik Molugu <104977337+HarikowshikMolugu@users.noreply.github.com>
2023-08-29 20:14:15 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,12 @@
 const Sequelize = require("sequelize");
 const config = require("../config/config.json");
+const pg = require('pg');
 const sequelize = new Sequelize('verceldb', 'default', 'A0WNIa9DvLPi', {
   host: 'ep-rough-shape-68710479-pooler.ap-southeast-1.postgres.vercel-storage.com',
   port: 5432,
   dialect: 'postgres',
   ssl: true,
+  dialectModule: pg,
 });
 
 // Test the connection
